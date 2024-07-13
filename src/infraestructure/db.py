2393647a2849d86a36328a646d48a80d073cd656
DiegoAP5 <213475@ids.upchiapas.edu.mx>
2024-07-12 19:56:57 -0600

    .env changes

--- src/infraestructure/db.py
+++ src/infraestructure/db.py
@@ -4,10 +4,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Obtener datos de conexión desde variables de entorno
-DB_USER = os.getenv('DB_USER', 'default_user')
-DB_PASSWORD = os.getenv('DB_PASSWORD', 'default_password')
-DB_HOST = os.getenv('DB_HOST', 'localhost')
-DB_NAME = os.getenv('DB_NAME', 'inventory')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_NAME = os.getenv('DB_NAME')
 
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 
